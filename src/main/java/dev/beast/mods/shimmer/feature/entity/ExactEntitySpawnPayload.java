9faf80e6d2f1708212d6059e1f41781d12b92f8f
LatvianModder <latvianmodder@gmail.com>
2025-03-28 14:30:42 +0200

    Moved streamcodec of ExactEntitySpawnPayload out

--- src/main/java/dev/beast/mods/shimmer/feature/entity/ExactEntitySpawnPayload.java
+++ src/main/java/dev/beast/mods/shimmer/feature/entity/ExactEntitySpawnPayload.java
@@ -7,8 +7,10 @@ import dev.beast.mods.shimmer.feature.codec.ShimmerStreamCodecs;
 import dev.beast.mods.shimmer.feature.net.ShimmerPacketPayload;
 import dev.beast.mods.shimmer.feature.net.ShimmerPacketType;
 import dev.beast.mods.shimmer.feature.net.ShimmerPayloadContext;
+import io.netty.buffer.ByteBuf;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.server.level.ServerEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -27,8 +29,7 @@ public record ExactEntitySpawnPayload(
 	float yHeadRot,
 	int data
 ) implements ShimmerPacketPayload {
-	@AutoPacket
-	public static final ShimmerPacketType<ExactEntitySpawnPayload> TYPE = ShimmerPacketType.internal("exact_entity_spawn", CompositeStreamCodec.of(
+	public static final StreamCodec<ByteBuf, ExactEntitySpawnPayload> STREAM_CODEC = CompositeStreamCodec.of(
 		ShimmerStreamCodecs.VIDEO_ID.map(BuiltInRegistries.ENTITY_TYPE::getValue, t -> t.builtInRegistryHolder().getKey().location()), ExactEntitySpawnPayload::type,
 		ShimmerStreamCodecs.UUID, ExactEntitySpawnPayload::uuid,
 		ByteBufCodecs.VAR_INT, ExactEntitySpawnPayload::id,
@@ -39,7 +40,10 @@ public record ExactEntitySpawnPayload(
 		ByteBufCodecs.FLOAT, ExactEntitySpawnPayload::yHeadRot,
 		ByteBufCodecs.VAR_INT, ExactEntitySpawnPayload::data,
 		ExactEntitySpawnPayload::new
-	));
+	);
+
+	@AutoPacket
+	public static final ShimmerPacketType<ExactEntitySpawnPayload> TYPE = ShimmerPacketType.internal("exact_entity_spawn", STREAM_CODEC);
 
 	public ExactEntitySpawnPayload(Entity entity, ServerEntity serverEntity, int data) {
 		this(
