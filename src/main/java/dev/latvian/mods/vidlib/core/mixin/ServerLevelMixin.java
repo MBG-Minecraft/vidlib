66af7681409fb800e3354718d843c5638c85fd85
TheLimeGlass <seantgrover@gmail.com>
2025-09-18 13:03:10 -0600

    Fix fake player additions from other mods

--- src/main/java/dev/latvian/mods/vidlib/core/mixin/ServerLevelMixin.java
+++ src/main/java/dev/latvian/mods/vidlib/core/mixin/ServerLevelMixin.java
@@ -2,16 +2,21 @@ package dev.latvian.mods.vidlib.core.mixin;
 
 import dev.latvian.mods.vidlib.core.VLChunkMap;
 import dev.latvian.mods.vidlib.core.VLServerLevel;
+import dev.latvian.mods.vidlib.core.VLServerPacketListener;
 import dev.latvian.mods.vidlib.feature.misc.CreateFireworksPayload;
 import dev.latvian.mods.vidlib.feature.prop.ServerProps;
+import dev.latvian.mods.vidlib.feature.session.ServerSessionData;
 import dev.latvian.mods.vidlib.feature.zone.ActiveZones;
 import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
 import it.unimi.dsi.fastutil.longs.LongSet;
 import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.network.Connection;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerChunkCache;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.CommonListenerCookie;
 import net.minecraft.world.item.component.FireworkExplosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.dimension.DimensionType;
@@ -22,6 +27,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
@@ -93,5 +101,16 @@ public abstract class ServerLevelMixin extends Level implements VLServerLevel {
 		}
 
 		return vl$isReplayLevel;
+	}
+
+	// Other mods like Motion Capture mod create fake players, so this avoid a null pointer.
+	@Inject(method = "addPlayer", at = @At("HEAD"))
+	private void vl$addPlayer(ServerPlayer player, CallbackInfo ci) {
+		if (player.vl$sessionData() != null) {
+			return;
+		}
+		ServerSessionData newPlayerSession = new ServerSessionData(player.getUUID());
+		((VLServerPacketListener) player.connection).vl$sessionData(newPlayerSession);
+		newPlayerSession.load(player);
 	}
 }
