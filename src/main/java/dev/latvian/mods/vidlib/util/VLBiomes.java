d4cdf9aade889017d8fb2c18a5bb7f351b1137af
LatvianModder <latvianmodder@gmail.com>
2026-02-16 18:46:15 +0200

    Added VLBiomes.PLAINS

--- src/main/java/dev/latvian/mods/vidlib/util/VLBiomes.java
+++ src/main/java/dev/latvian/mods/vidlib/util/VLBiomes.java
@@ -1,25 +1,258 @@
 package dev.latvian.mods.vidlib.util;
 
+import com.google.gson.JsonObject;
 import com.mojang.serialization.JsonOps;
 import dev.latvian.mods.klib.util.Lazy;
-import dev.latvian.mods.vidlib.VidLib;
-import dev.latvian.mods.vidlib.feature.platform.PlatformHelper;
-import net.minecraft.server.packs.PackType;
 import net.minecraft.world.level.biome.Biome;
 
-import java.nio.file.Files;
-
 public interface VLBiomes {
-	Lazy<Biome> VOID = Lazy.of(() -> {
-		try {
-			var file = PlatformHelper.CURRENT.findFile(PackType.SERVER_DATA, VidLib.id("worldgen/biome/void.json"));
+	Lazy<Biome> PLAINS = Lazy.of(() -> {
+		var json = JsonUtils.GSON.fromJson("""
+			{
+				"carvers": [
+					"minecraft:cave",
+					"minecraft:cave_extra_underground",
+					"minecraft:canyon"
+				],
+				"downfall": 0.4,
+				"effects": {
+					"fog_color": 12638463,
+					"mood_sound": {
+						"block_search_extent": 8,
+						"offset": 2.0,
+						"sound": "minecraft:ambient.cave",
+						"tick_delay": 6000
+					},
+					"music_volume": 1.0,
+					"sky_color": 7907327,
+					"water_color": 4159204,
+					"water_fog_color": 329011
+				},
+				"features": [
+					[],
+					[
+						"minecraft:lake_lava_underground",
+						"minecraft:lake_lava_surface"
+					],
+					[
+						"minecraft:amethyst_geode"
+					],
+					[
+						"minecraft:monster_room",
+						"minecraft:monster_room_deep"
+					],
+					[],
+					[],
+					[
+						"minecraft:ore_dirt",
+						"minecraft:ore_gravel",
+						"minecraft:ore_granite_upper",
+						"minecraft:ore_granite_lower",
+						"minecraft:ore_diorite_upper",
+						"minecraft:ore_diorite_lower",
+						"minecraft:ore_andesite_upper",
+						"minecraft:ore_andesite_lower",
+						"minecraft:ore_tuff",
+						"minecraft:ore_coal_upper",
+						"minecraft:ore_coal_lower",
+						"minecraft:ore_iron_upper",
+						"minecraft:ore_iron_middle",
+						"minecraft:ore_iron_small",
+						"minecraft:ore_gold",
+						"minecraft:ore_gold_lower",
+						"minecraft:ore_redstone",
+						"minecraft:ore_redstone_lower",
+						"minecraft:ore_diamond",
+						"minecraft:ore_diamond_medium",
+						"minecraft:ore_diamond_large",
+						"minecraft:ore_diamond_buried",
+						"minecraft:ore_lapis",
+						"minecraft:ore_lapis_buried",
+						"minecraft:ore_copper",
+						"minecraft:underwater_magma",
+						"minecraft:disk_sand",
+						"minecraft:disk_clay",
+						"minecraft:disk_gravel"
+					],
+					[],
+					[
+						"minecraft:spring_water",
+						"minecraft:spring_lava"
+					],
+					[
+						"minecraft:glow_lichen",
+						"minecraft:patch_tall_grass_2",
+						"minecraft:patch_bush",
+						"minecraft:trees_plains",
+						"minecraft:flower_plains",
+						"minecraft:patch_grass_plain",
+						"minecraft:brown_mushroom_normal",
+						"minecraft:red_mushroom_normal",
+						"minecraft:patch_pumpkin",
+						"minecraft:patch_sugar_cane",
+						"minecraft:patch_firefly_bush_near_water"
+					],
+					[
+						"minecraft:freeze_top_layer"
+					]
+				],
+				"has_precipitation": true,
+				"spawn_costs": {},
+				"spawners": {
+					"ambient": [
+						{
+							"type": "minecraft:bat",
+							"maxCount": 8,
+							"minCount": 8,
+							"weight": 10
+						}
+					],
+					"axolotls": [],
+					"creature": [
+						{
+							"type": "minecraft:sheep",
+							"maxCount": 4,
+							"minCount": 4,
+							"weight": 12
+						},
+						{
+							"type": "minecraft:pig",
+							"maxCount": 4,
+							"minCount": 4,
+							"weight": 10
+						},
+						{
+							"type": "minecraft:chicken",
+							"maxCount": 4,
+							"minCount": 4,
+							"weight": 10
+						},
+						{
+							"type": "minecraft:cow",
+							"maxCount": 4,
+							"minCount": 4,
+							"weight": 8
+						},
+						{
+							"type": "minecraft:horse",
+							"maxCount": 6,
+							"minCount": 2,
+							"weight": 5
+						},
+						{
+							"type": "minecraft:donkey",
+							"maxCount": 3,
+							"minCount": 1,
+							"weight": 1
+						}
+					],
+					"misc": [],
+					"monster": [
+						{
+							"type": "minecraft:spider",
+							"maxCount": 4,
+							"minCount": 4,
+							"weight": 100
+						},
+						{
+							"type": "minecraft:zombie",
+							"maxCount": 4,
+							"minCount": 4,
+							"weight": 95
+						},
+						{
+							"type": "minecraft:zombie_villager",
+							"maxCount": 1,
+							"minCount": 1,
+							"weight": 5
+						},
+						{
+							"type": "minecraft:skeleton",
+							"maxCount": 4,
+							"minCount": 4,
+							"weight": 100
+						},
+						{
+							"type": "minecraft:creeper",
+							"maxCount": 4,
+							"minCount": 4,
+							"weight": 100
+						},
+						{
+							"type": "minecraft:slime",
+							"maxCount": 4,
+							"minCount": 4,
+							"weight": 100
+						},
+						{
+							"type": "minecraft:enderman",
+							"maxCount": 4,
+							"minCount": 1,
+							"weight": 10
+						},
+						{
+							"type": "minecraft:witch",
+							"maxCount": 1,
+							"minCount": 1,
+							"weight": 5
+						}
+					],
+					"underground_water_creature": [
+						{
+							"type": "minecraft:glow_squid",
+							"maxCount": 6,
+							"minCount": 4,
+							"weight": 10
+						}
+					],
+					"water_ambient": [],
+					"water_creature": []
+				},
+				"temperature": 0.8
+			}""", JsonObject.class);
+		return Biome.DIRECT_CODEC.parse(JsonOps.INSTANCE, json).getOrThrow();
+	});
 
-			try (var reader = Files.newBufferedReader(file)) {
-				var json = JsonUtils.read(reader);
-				return Biome.DIRECT_CODEC.parse(JsonOps.INSTANCE, json).getOrThrow();
+	Lazy<Biome> VOID = Lazy.of(() -> {
+		var json = JsonUtils.GSON.fromJson("""
+			{
+				"carvers": [],
+				"temperature": 0.5,
+				"downfall": 0.5,
+				"effects": {
+					"fog_color": 12638463,
+					"sky_color": 7907327,
+					"water_color": 4159204,
+					"water_fog_color": 329011,
+					"music_volume": 0.0
+				},
+				"features": [
+					[],
+					[],
+					[],
+					[],
+					[],
+					[],
+					[],
+					[],
+					[],
+					[],
+					[]
+				],
+				"has_precipitation": false,
+				"spawn_costs": {},
+				"spawners": {
+					"ambient": [],
+					"axolotls": [],
+					"creature": [],
+					"misc": [],
+					"monster": [],
+					"underground_water_creature": [],
+					"water_ambient": [],
+					"water_creature": []
+				}
 			}
-		} catch (Exception ex) {
-			throw new RuntimeException(ex);
-		}
+			""", JsonObject.class);
+		return Biome.DIRECT_CODEC.parse(JsonOps.INSTANCE, json).getOrThrow();
 	});
 }
