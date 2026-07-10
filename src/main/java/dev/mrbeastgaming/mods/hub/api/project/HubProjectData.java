00aaa9078d389cb88658c4defad9b93ce7788241
LatvianModder <latvianmodder@gmail.com>
2026-07-10 21:27:56 +0300

    Added "archived" to project data

--- src/main/java/dev/mrbeastgaming/mods/hub/api/project/HubProjectData.java
+++ src/main/java/dev/mrbeastgaming/mods/hub/api/project/HubProjectData.java
@@ -18,7 +18,8 @@ public record HubProjectData(
 	UInt64 discordGuild,
 	boolean visible,
 	List<HubTeamData> teams,
-	HubDataMap customData
+	HubDataMap customData,
+	boolean archived
 ) {
 	public static final Codec<HubProjectData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
 		Hex32.CODEC.fieldOf("id").forGetter(HubProjectData::id),
@@ -29,7 +30,8 @@ public record HubProjectData(
 		UInt64.CODEC.optionalFieldOf("discord_guild", UInt64.NONE).forGetter(HubProjectData::discordGuild),
 		Codec.BOOL.optionalFieldOf("visible", false).forGetter(HubProjectData::visible),
 		HubTeamData.CODEC.listOf().optionalFieldOf("teams", List.of()).forGetter(HubProjectData::teams),
-		HubDataMap.CODEC.optionalFieldOf("custom_data", HubDataMap.EMPTY).forGetter(HubProjectData::customData)
+		HubDataMap.CODEC.optionalFieldOf("custom_data", HubDataMap.EMPTY).forGetter(HubProjectData::customData),
+		Codec.BOOL.optionalFieldOf("archived", false).forGetter(HubProjectData::archived)
 	).apply(instance, HubProjectData::new));
 
 	public static HubProjectData PACK = null;
