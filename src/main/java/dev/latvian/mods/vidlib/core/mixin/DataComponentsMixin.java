8c0676c00c62de2867de93928406f68966286b6a
LatvianModder <latvianmodder@gmail.com>
2026-07-25 02:47:19 +0300

    Added no bob list for items

--- src/main/java/dev/latvian/mods/vidlib/core/mixin/DataComponentsMixin.java
+++ src/main/java/dev/latvian/mods/vidlib/core/mixin/DataComponentsMixin.java
@@ -1,13 +1,7 @@
 package dev.latvian.mods.vidlib.core.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.mojang.serialization.Codec;
-import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.world.item.component.LodestoneTracker;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
@@ -16,10 +10,5 @@ public class DataComponentsMixin {
 	@ModifyConstant(method = "lambda$static$1", constant = @Constant(intValue = 99))
 	private static int vl$maxSlotSize(int original) {
 		return 1_000_000_000;
-	}
-
-	@WrapOperation(method = "lambda$static$58", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/component/DataComponentType$Builder;persistent(Lcom/mojang/serialization/Codec;)Lnet/minecraft/core/component/DataComponentType$Builder;"))
-	private static DataComponentType.Builder<LodestoneTracker> vl$lodestone(DataComponentType.Builder<LodestoneTracker> instance, Codec<LodestoneTracker> codec, Operation<DataComponentType.Builder<LodestoneTracker>> original) {
-		return original.call(instance, codec);
 	}
 }
